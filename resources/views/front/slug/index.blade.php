38bde2159beb1a604017fe4efe21fd60bb16a582
Andrew <code70@inbox.ru>
2023-11-27 16:29:09 +0700

    intermediate

--- resources/views/front/slug/index.blade.php
+++ resources/views/front/slug/index.blade.php
@@ -4,53 +4,39 @@
 @section('desc', 'Описание для страницы услуги')
 
 @section('content')
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="banner p-4 border border-dark rounded bg-dark text-light d-flex align-items-end position-relative" 
-                        style="
-                            min-height: 350px; 
-                            background: url({{ asset('/assets/images/banner.jpg') }});
-                            background-position: center !important
-
-                        ">
-                        <div class="overlay"></div>
-                        <div class="d-block position-relative">
-                            <h2 class="mb-0">Строительные услуги</h2>
-                            <p>Наша компания оказывает широкий спектр стрительных услуг</p>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-4">
+                <a href="" class="slug-link">
+                    <div class="slug-item overflow-hidden position-relative h-100 rounded bg-image p-4" style="background: url({{ asset('/assets/images/banner.jpg') }})">
+                        <div class="overlay" style="background: rgba(0, 0, 0, .7)"></div>
+                        <div class="d-block align-self-end position-relative px-2 pb-2">
+                            <h3 class="m-0">Строительные услуги</h3>
+                            <p>Оказание строительных услуг в городе томске и области</p>
                         </div>
                     </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-12 col-lg-8">
+                <div class="row g-2">
+                    @foreach ($slugs as $slug)
+                        <div class="col-12 col-md-4">
+                            <a href="" class="slug-link">
+                                <div class="slug-item overflow-hidden position-relative rounded">
+                                    <img src="{{ asset('/assets/images/banner.jpg') }}" alt="" class="image-fluid">
+                                    <div class="overlay"></div>
+                                    <div class="d-block align-self-end position-relative px-2 pb-2">
+                                        <h4 class="m-0">{{ $slug->name }}</h4>
+                                        <p> {{ $slug->desc }} </p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>    
+                    @endforeach
                 </div>
             </div>
         </div>
-    </section>
-    <section>
-        <div class="container">
-            <div class="row mb-4">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h2>Услуги</h2>
-                        <hr class="w-50">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                @foreach ($slugs as $slug)
-                    <div class="col-12 col-md-6 col-lg-4 mb-4">
-                        <div class="single-slug p-4 d-flex align-items-end position-relative" style="
-                            background: url({{ asset('/assets/images/banner.jpg') }});   
-                            background-position: center !important
-                        ">
-                            <div class="overlay"></div>
-                            <div class="d-block text-light position-relative">
-                                <h4 class="mb-0">{{ $slug->name }}</h4>
-                                <p> {{ $slug->desc }} </p>    
-                            </div>
-                        </div>
-                    </div>    
-                @endforeach
-            </div>
-        </div>
-    </section>
+    </div>
+</section>
 @endsection
