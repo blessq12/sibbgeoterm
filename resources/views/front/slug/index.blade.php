a5aec898ef714586844a89050f8439cec8353ef6
Andrew <code70@inbox.ru>
2023-12-13 22:04:02 +0700

    intermediate

--- resources/views/front/slug/index.blade.php
+++ resources/views/front/slug/index.blade.php
@@ -11,7 +11,7 @@
             <a href="{{ route('main.slug.single-slug', $slug->uri) }}">
                 <div class="col">
                     <div class="position-relative d-flex align-items-end px-2 py-4 mb-4 overflow-hidden rounded" style="min-height: 200px">
-                        <img src="{{ optional($slug->thumb)->path ?? '//via.placeholder.com/512x512' }}" alt="" class="img-fluid position-absolute top-0 w-100" style="left:0">
+                        <img src="{{ optional($slug->thumb)->path ?? '//via.placeholder.com/512x512' }}" alt="" class="img-fluid position-absolute align-self-center w-100" style="left:0">
                         <div class="overlay"></div>
                             <div class="position-relative">
                                 <h4 class="mb-0 fs-5">{{ $slug->name }}</h4>
