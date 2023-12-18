58f0f127e96d5bb62910baaea112cfd7f9f3c62e
Andrew <code70@inbox.ru>
2023-12-18 13:36:06 +0700

    intermediate

--- resources/views/front/construct/index.blade.php
+++ resources/views/front/construct/index.blade.php
@@ -9,14 +9,14 @@
             <div class="col-12 col-md-12 col-lg-6">
                 <a href="{{ route('main.construct.angary') }}">
                     <div class="construct-item text-center">
-                        <h2>Ангары арочного типа</h2>
+                        <h2>Строительство безкарскасных ангаров арочного типа</h2>
                     </div>
                 </a>
             </div>
             <div class="col-12 col-md-12 col-lg-6">
                 <a href="{{ route('main.construct.sandwich') }}">
                     <div class="construct-item text-center">
-                        <h2>Строительство и проектиирование складских и промышленных зданий</h2>
+                        <h2>Строительство и проектирование складских и промышленных зданий</h2>
                     </div>
                 </a>
             </div>
