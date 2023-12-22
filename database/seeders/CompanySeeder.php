4f84917d5afce3fc779b09c3af9957e359e2f330
Andrew <code70@inbox.ru>
2023-12-22 16:55:37 +0700

    intermediate

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -17,9 +17,9 @@ class CompanySeeder extends Seeder
             'name' => 'сибгеотерм',
             'description' => 'Строительство быстровозводимых зданий в Томске, Кемерово и Красноярске',
             'logo' => '/assets/images/logo.png',
-            'tel' => '+7 (913) 820-33-05',
+            'tel' => '+7 (3822) 503 305',
             'email' => 'sibgeotherm@yandex.ru',
-            'address' => 'ул. Учебная, 37а, 2 подъезд, офис 13',
+            'address' => 'Нахимова 8 стр 11',
             'navigator' => 'https://yandex.ru/maps/67/tomsk/?ll=84.960890%2C56.458590&mode=routes&rtext=~56.458641%2C84.960378&rtt=auto&ruri=~ymapsbm1%3A%2F%2Forg%3Foid%3D65006678309&z=18.59',
             'waLink' => '',
             'tgLink' => ''
