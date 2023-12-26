fa784be6390b787d809f76f4ff744afc75206d4e
Andrew <code70@inbox.ru>
2023-12-26 21:18:59 +0700

    intermediate

--- resources/js/components/PageSlider.vue
+++ resources/js/components/PageSlider.vue
@@ -4,11 +4,25 @@ export default{
         images: Object,
         title: String,
         subtitle: String
-    }
+    },
+    data:()=>({
+        overlay: false,
+        current: null
+    })
 }
 </script>
 
 <template>
+    <div class="invisible position-fixed top-0 w-100 h-100 p-2 p-md-5" v-if="overlay" style="z-index: 20;">
+        <div class="container h-100">
+            <div class="row row-cols justify-content-center h-100 align-items-center">
+                <div class="col text-center position-relative" style="z-index: 21;">
+                    <img :src="current" alt="" class="img-fluid position-relative visible" style="z-index: 25;">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="overlay" v-if="overlay" @click="overlay=!overlay"></div>
     <div class="container">
         <div class="row mb-4">
             <div class="col-12">
@@ -27,7 +41,13 @@ export default{
                         <i class="cursor-pointer visible fa fa-caret-right fa-4x text-red h-100 d-flex align-items-center p-3" style="z-index: 1;" @click="$refs.slider.scrollLeft += 424"></i>
                     </div>
                     <ul class="image-list" ref="slider" v-if="images.length">
-                        <li v-for="image in images" :key="image.id" :style="'background: url('+ image.path +')'"></li>
+                        <li 
+                            v-for="image in images" 
+                            :key="image.id" 
+                            :style="'background: url('+ image.path +')'" 
+                            class="cursor-pointer"
+                            @click="overlay=!overlay; current = image.path"
+                        ></li>
                     </ul>
                     <ul class="image-list placeholder-glow" ref="slider" v-else>
                         <li v-for="e in 10" class="placeholder"></li>
@@ -39,6 +59,13 @@ export default{
 </template>
 
 <style lang="sass" scoped>
+.overlay
+    position: fixed
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: 10
 .image-list
     display: flex
     align-items: center
@@ -48,8 +75,11 @@ export default{
     &::-webkit-scrollbar
         display: none
     li
-        min-width: 400px
-        height: 400px
+        min-width: 280px
+        height: 250px
+        @media (min-width: 768px)
+            min-width: 400px
+            height: 400px
         margin-right: 24px
         border-radius: $default-radius
         padding: 12px
